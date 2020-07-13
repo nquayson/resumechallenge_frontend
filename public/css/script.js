cd7cf3b9df858afa3c4266375b1f955030a893e8
Nana <nanayaw.net@live.com>
2020-07-13 02:19:55 -0400

    content updates

--- public/css/script.js
+++ public/css/script.js
@@ -2,9 +2,8 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        h=JSON.parse(xhttp.responseText);
-       document.getElementById("hits").innerHTML = h.count;
+       document.getElementById("hits").innerHTML = xhttp.responseText;
     }
 };
-xhttp.open("GET", "https://4zk0zxf6rk.execute-api.us-east-2.amazonaws.com/dev/MyResource", true);
+xhttp.open("GET", "https://7u04hfdb9e.execute-api.us-east-2.amazonaws.com/prod/num", true);
 xhttp.send();
